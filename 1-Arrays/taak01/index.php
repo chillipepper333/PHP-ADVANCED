6050519ba70f1bbfec5908b8a3fc914e01e58bbd
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-01-12 11:13:33 +0100

    php advanced 1-1

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam['Piet'] = 'Piet';
+$voornaam['Mohammed'] = 'Mohammed';
+$voornaam['Els'] = 'Els';
+
+$achternaam['Piet'] = 'Post';
+$achternaam['Mohammed'] = 'Ali';
+$achternaam['Els'] = 'Wijfjes';
+
+$leeftijd['Piet'] = 16;
+$leeftijd['Mohammed'] = 17;
+$leeftijd['Els'] = 18;
+
+$klas['Piet'] = '1A';
+$klas['Mohammed'] = '2B';
+$klas['Els'] = '3C';
+
+echo "gegevens van piet voornaam: ".$voornaam['Piet']." achternaam: ".$achternaam['Piet']." Leeftijd: ".$leeftijd['Piet']." Klas: ".$klas['Piet']."";
+?>
+
+</body>
+</html> 
