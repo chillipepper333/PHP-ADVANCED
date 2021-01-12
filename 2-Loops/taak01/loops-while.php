4762ba7922acede68375f23799d0bf5cc120f350
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-01-12 11:34:12 +0100

    php advanced 2-1

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$leeftijd = 0;
+
+while($leeftijd < 18){
+    echo "<li>Ik ben $leeftijd jaar dus ik mag nog niet stemmen </li>";
+    $leeftijd++;
+}
+echo "ik ben 18 dus ik heb stemrecht";
+?>
+
+</body>
+</html> 
