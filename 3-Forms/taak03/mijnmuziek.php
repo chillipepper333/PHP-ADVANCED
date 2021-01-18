4ff51daeb55f74d0e22051d9f96695c9f827f524
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-01-18 14:29:49 +0100

    php advance 3-3

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+
+<body>
+
+<?php
+
+echo "<li>artiest is ".$_GET["artiest"];
+echo "</li><li>titel is ".$_GET["titel"];
+echo "</li><li>jaar van uitkomst is ".$_GET["jaar"];
+echo "</li><li>muzieksoort is ".$_GET["muzieksoort"]."</li>";
+
+?>
+
+</body>
+
+</html>
