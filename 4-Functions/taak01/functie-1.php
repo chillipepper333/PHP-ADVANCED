016e8fe6605bbf6f6038129cfd5bcc6869472a17
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-01-19 12:02:40 +0100

    php advanced 4-1

---
+++ 4-Functions/taak01/functie-1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+
+<body>
+
+
+<?php
+function schrijfkleur($kleur){
+    echo "dit is de kleur $kleur";
+
+}
+
+schrijfkleur("red")  ;
+
+function schrijfkleur1($kleur1){
+    echo "dit is de kleur $kleur1";
+
+}
+
+schrijfkleur1("yellow")  ;
+
+function schrijfkleur2($kleur2){
+    echo "dit is de kleur $kleur2";
+
+}
+
+schrijfkleur2("blue")  ;
+
+function schrijfkleur3($kleur3){
+    echo "dit is de kleur $kleur3";
+
+}
+
+schrijfkleur3("green")  ;
+
+
+?>
+
+
+
+</body>
+
+</html>
